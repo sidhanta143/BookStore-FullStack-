e6cebb10d5b76c448e75eaeac9a2b7c8b98f7ec7
G Sidhanta Dora <sidhantadora143@gmail.com>
2026-02-16 22:04:26 +0530

    Delete Route/user.router.js

--- Route/user.router.js
+++
@@ -1,7 +0,0 @@
-// import { signup } from "../Controller/user.controller";
-import { signup } from "../Controller/user.controller";
-// import express, { Router } from "express";
-import express from "express"
-const router=express.Router();
-router.post("/signup",signup);
-export default user.router
